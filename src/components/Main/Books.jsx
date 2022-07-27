5706b5585426027ebed62d715cda12852d564c5f
Dima <78321515+CommanderCoolDev@users.noreply.github.com>
2022-07-27 12:43:16 +0300

    feat: added export csv btn

--- src/components/Main/Books.jsx
+++ src/components/Main/Books.jsx
@@ -1,18 +1,46 @@
 import Book from './Book';
 import Spinner from '../Spinner/Spinner';
 import Hero from '../Hero/Hero';
+import { CSVLink } from 'react-csv';
 
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 const Books = ({ books }) => {
-  //   console.log(books);
+  const [csvData, setCsvData] = useState([]);
+  const data = books.map(book => book.volumeInfo);
+  const headers = [
+    { label: 'Name', key: 'title' },
+    // { label: 'Author', key: 'authors' },
+    // { label: 'Publisher', key: 'publisher' },
+    // { label: 'Published Date', key: 'publishedDate' },
+    // { label: 'Categories', key: 'categories' },
+    // { label: 'Page Count', key: 'pageCount' },
+    // { label: 'Description', key: 'description' },
+  ];
+  const csvReport = {
+    filename: 'Report.csv',
+    headers: headers,
+    data: csvData,
+  };
+  // console.log(books);
+  useEffect(() => {
+    setCsvData(data);
+  }, []);
+  console.log(csvData);
 
   return (
-    <div className="books">
-      {books.map(book => (
-        <Book key={book.id} book={book} />
-      ))}
-    </div>
+    <>
+      <div className="csv-box">
+        <CSVLink {...csvReport} className="btn lime lighten-1">
+          Export to CSV
+        </CSVLink>
+      </div>
+      <div className="books">
+        {books.map(book => (
+          <Book key={book.id} book={book} />
+        ))}
+      </div>
+    </>
   );
 };
 export default Books;
