425d93fdce15f0322202cc7647bdf90d3986ac63
Dima Glebov <78321515+CommanderCoolDev@users.noreply.github.com>
2022-08-13 18:26:37 +0300

    feat:added suspense

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,64 @@
+import { lazy, Suspense } from 'react';
 import { ToastContainer, Flip } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Route, Routes } from 'react-router-dom';
-import MainView from './views/MainView/MainView';
+
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
-import Selected from './components/Selected/Selected';
-import NotFoundView from './views/NotFoundView/NotFoundView';
-import Shelf from './components/Shelf/Shelf';
-import Books from './components/Main/Books';
 
+import Spinner from './components/Spinner/Spinner';
 import LoginView from './views/AuthView/LoginView';
 import RegisterView from './views/AuthView/RegisterView';
 import PrivateRoute from './components/Routes/PrivateRoute';
+const MainView = lazy(() => import('./views/MainView/MainView'));
+const Selected = lazy(() => import('./components/Selected/Selected'));
+const Books = lazy(() => import('./components/Main/Books'));
+const Shelf = lazy(() => import('./components/Shelf/Shelf'));
+const NotFoundView = lazy(() => import('./views/NotFoundView/NotFoundView'));
 
 function App() {
   return (
     <div className="bg">
       <Header />
-      <Routes>
-        <Route path="/login" element={<LoginView />} />
-        <Route path="/register" element={<RegisterView />} />
-        <Route
-          path="/figli-book-app"
-          element={
-            <PrivateRoute>
-              <MainView />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/books"
-          element={
-            <PrivateRoute>
-              <Books />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/book/:id"
-          element={
-            <PrivateRoute>
-              <Selected />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/shelf"
-          element={
-            <PrivateRoute>
-              <Shelf />
-            </PrivateRoute>
-          }
-        />
-        <Route path="*" element={<NotFoundView />} />
-      </Routes>
+      <Suspense fallback={<Spinner />}>
+        <Routes>
+          <Route path="/login" element={<LoginView />} />
+          <Route path="/register" element={<RegisterView />} />
+          <Route
+            path="/figli-book-app"
+            element={
+              <PrivateRoute>
+                <MainView />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/books"
+            element={
+              <PrivateRoute>
+                <Books />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/book/:id"
+            element={
+              <PrivateRoute>
+                <Selected />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/shelf"
+            element={
+              <PrivateRoute>
+                <Shelf />
+              </PrivateRoute>
+            }
+          />
+          <Route path="*" element={<NotFoundView />} />
+        </Routes>
+      </Suspense>
       <ToastContainer
         position="top-right"
         autoClose={2000}
