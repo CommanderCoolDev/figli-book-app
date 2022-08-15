d077bbd3aa08a9f3aecf9e95a28a33225bf1abfa
Dima <78321515+CommanderCoolDev@users.noreply.github.com>
2022-08-15 11:33:09 +0300

    feat: spinner updtd

--- src/components/Spinner/Spinner.jsx
+++ src/components/Spinner/Spinner.jsx
@@ -2,7 +2,7 @@ import { PacmanLoader } from 'react-spinners';
 
 export default function Spinner() {
   return (
-    <div className="spinner">
+    <div className="spinner content">
       <PacmanLoader color="#9e9d24" />
     </div>
   );
